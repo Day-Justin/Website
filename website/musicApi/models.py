da0d41e07396ea1786ab04ed0354ec607321d55e
JD <dayjustin.jd@gmail.com>
2023-10-03 18:13:39 -0400

    server side create room

--- website/musicApi/models.py
+++ website/musicApi/models.py
@@ -17,10 +17,10 @@ def gen_unique_code(): # unique room code generator that checks all room objects
 
 
 class Room(models.Model):
-    rm_code = models.CharField(max_length=8, default="", unique=True)
+    rm_code = models.CharField(max_length=8, default=gen_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
-    vote_to_skip = models.IntegerField(null=False, default=1)
+    votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
     
--- website/musicApi/urls.py
+++ website/musicApi/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 # don't forget to include in urls.py in main folder
 
 
 urlpatterns = [
     path('home', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view()),
 ]
